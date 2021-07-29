45f9b0dcea5533592f3b59adc19843d6a28f89e5
qzqz0826 <qzqz0826@cavuai.com>
2021-07-29 16:27:05 +0900

    [ADD] 스프링 부트 필터 내용 정리

---
+++ filter/src/main/java/com/example/filter/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.example.filter.controller;
+
+import com.example.filter.dto.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping
+    public User user(@RequestBody User user){
+        return user;
+    }
+}
